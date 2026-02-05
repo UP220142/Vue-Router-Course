62c5f0f96c88d92f0b42ff25b89e66e74982d4a5
Vegetta777 <up220142@alumnos.upa.edu.mx>
2026-02-05 09:30:49 -0600

    feat :sparkles: PokeAPI usage and Pokemon list routing

--- src/router/index.js
+++ src/router/index.js
@@ -7,15 +7,22 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView,
+      component: () => import('../views/HomeView.vue'),
     },
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
+    },
+    {
+      path: '/pokemons',
+      name: 'pokemons',
+      component: () => import('../views/PokemonsView.vue'),
+    },
+    {
+      path: '/pokemons/:name',
+      name: 'poke',
+      component: () => import('../views/PokeView.vue'),
     },
   ],
 })
